ad8707a37facc44f6d766a7c367d104c1353e690
tiendat <dat.70.k3@gmail.com>
2024-11-29 11:08:24 +0700

    Update SS8_01.cpp

--- SS8_01.cpp
+++ SS8_01.cpp
@@ -1,28 +1,17 @@
-#include<stdio.h>
-int main(){
-	// Khai bao va nhap cac phan tu cho mang so nguyen
-	// B1: Khai bao va nhap gia tri cho rows va cols
-	int a, b;
-	printf("Nhap gia tri rows cho mang: ");
-	scanf("%d", &a);
-	printf("Nhap gia tri cols cho mang: ");
-	scanf("%d", &b);
-	// B2: Khai bao array[rows][cols]
-	int array[a][b];
-	// B3: Nhap cac phan tu cho mang hai chieu
-	for(int i=0; i<a; i++){
-		for(int j=0; j<b; j++){
-			printf("array[%d][%d]", i, j);
-			scanf("%d", &array[i][j]);
-		}
-	}
-	// B4: dung vong lap in cac phan tu cua mang hai chieu
-	printf("Phan tu trong mang la:\n");
-	for(int i=a-1; i>=0; i--){
-		for(int j=b-1; j>=0; j--){
-			printf("%2d", array[i][j]);
-		}
-		printf("\n");
-	}
-	return 0;
+#include <stdio.h>
+
+int main() {
+   
+    int array[] = {5, 6, 7, 8, 9};
+    int n = sizeof(arr) / sizeof(arr[0]); 
+
+   
+    printf("Cac phan tu trong mang tu cuoi ve dau:\n");
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
+
