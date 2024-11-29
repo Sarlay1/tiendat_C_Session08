982ddff41d27e9ad8bf2bacdab52b999a6487314
tiendat <dat.70.k3@gmail.com>
2024-11-29 11:09:59 +0700

    Update SS8_01.cpp

--- SS8_01.cpp
+++ SS8_01.cpp
@@ -3,12 +3,12 @@
 int main() {
    
     int array[] = {5, 6, 7, 8, 9};
-    int n = sizeof(arr) / sizeof(arr[0]); 
+    int n = sizeof(array) / sizeof(array[0]); 
 
    
     printf("Cac phan tu trong mang tu cuoi ve dau:\n");
     for (int i = n - 1; i >= 0; i--) {
-        printf("%d ", arr[i]);
+        printf("%d ", array[i]);
     }
     printf("\n");
 
